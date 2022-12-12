8da3461adf7b8bc3871421116f6f329b8d53b2b9
evgeniy.filatov <evgeniy.filatov.gb@gmail.com>
2022-12-12 13:07:29 +0300

    добавили задачу 27

---
+++ task27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Введите любое число : ");
+int A = int.Parse(Console.ReadLine()!);
+
+int CountSumofChar(int number)
+{
+    int average = 0;
+    int result = 0;
+    for (int i = 0; i <= number; i++)
+    {
+        average = number - number % 10;
+        result = result + (number - average);
+        number = number / 10;
+    }
+    return result;
+}
+
+Console.WriteLine($"Сумма цифр в числе = {CountSumofChar(A)}");
